ea0239ac5c0b85502820385813b8a8f250ebfc23
SOULEYMAN ALI Youssouf <souleymay@isl-edu.be>
2026-06-15 15:48:41 +0200

    fix: correction des conflits Angular, application fonctionnelle

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-<<<<<<< HEAD
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
-=======
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
 import { provideHttpClient } from '@angular/common/http';
-import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideHttpClient()]
+  providers: [
+    provideRouter(routes),
+    provideHttpClient()
+  ]
 }).catch(err => console.error(err));
->>>>>>> f76e1179805d20a0b4910997d154f17a3b9adcc8
